e305e3dfa366f99dfc5c2bc2062839f6b6395b00
radhika <rvramayra1998@gmail.com>
2022-05-02 17:18:11 +0530

    add

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,17 @@
 
 export const Home = () => {
-  const [user,setUser] =useState([])
-  useEffect(()=>{
+//   const [user,setUser] =useState([])
+//   useEffect(()=>{
       
-     axios.get("http://localhost:8080/users").then(({data})=>{
-      console.log("data",data)
-      setUser(data)
-     })
+//      axios.get("http://localhost:8080/users").then(({data})=>{
+//       console.log("data",data)
+//       setUser(data)
+//      })
 
-  },[]);
+//   },[]);
 
- var totalemp= user.length
-  console.log(user)
+//  var totalemp= user.length
+//   console.log(user)
   return (
     <div className="homepage">
      <h1> Welcome to Laptop service center. Please login to continue</h1>,
